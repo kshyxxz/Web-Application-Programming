509299ce2e394da390fc6866d75090024d466c8f
kshyxxz <kshyxxz@gmail.com>
2026-02-21 19:35:20 +0545

    feat: add CRUD views and templates for Todo items

--- lab5_TodoList-django/todoApp/todoApp/urls.py
+++ lab5_TodoList-django/todoApp/todoApp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('list_app.urls')),
+    path('', include(('list_app.urls', 'list_app'), namespace='list_app')),
 ]
