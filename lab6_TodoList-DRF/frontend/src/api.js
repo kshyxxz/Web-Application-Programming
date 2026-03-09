9e6f273727d6aec024976d9e1f5f1ad8f1c8777f
kshyxxz <kshyxxz@gmail.com>
2026-03-09 22:16:35 +0545

    refactor(api): update API baseURL to use environment variable

--- lab6_TodoList-DRF/frontend/src/api.js
+++ lab6_TodoList-DRF/frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000/api" });
+const API = axios.create({
+	baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+});
 
 export const fetchTodos = () => API.get("/todos/");
 export const createTodo = (data) => API.post("/todos/", data);
